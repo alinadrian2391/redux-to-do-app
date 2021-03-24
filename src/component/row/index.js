945e07ce0e3f8d346e85654477a6689e7ede0d22
nlopez <nlopez@nanthealth.com>
2021-03-24 13:28:12 -0400

    ui styling for rows

--- src/component/row/index.js
+++ src/component/row/index.js
@@ -2,8 +2,10 @@ import Button from '../button'
 import './style.css'
 const row = props => {
     const {title} = props;
-    return  <div className="flex">
-                <span>{title}</span>
+    return  <div className="row item">
+                <div className="col-6">
+                    <span>{title}</span>
+                </div>
                 <Button type = "Edit" />
                 <Button type = "Delete" />
             </div>
